9cc94d3b138d50e93e77da21b5f6dc3c95936ecd
Isma_ <studiogalactik@gmail.com>
2022-12-16 12:30:54 +0100

    sidebar in layout for /dashboard routes

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,8 +1,18 @@
 <template>
-    <div class="bg-primary">
-        <div class="avatar">
+    <div class="bg-primary flex flex-col justify-between items-center w-20 py-5">
+        <div class="avatar w-2/3 h-fit">
             <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img src="https://via.placeholder.com/75"/>
+                <img src="https://via.placeholder.com/500" />
+            </div>
+        </div>
+        <div>
+            <div>Home</div>
+        </div>
+        <div>
+            <div class="avatar placeholder">
+                <div class="bg-neutral-focus text-neutral-content rounded-full w-12">
+                    <span>MX</span>
+                </div>
             </div>
         </div>
     </div>
