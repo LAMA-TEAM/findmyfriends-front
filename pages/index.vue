00b9419c55553d6c744b71ccb71f5574ff8fb71a
Isma_ <studiogalactik@gmail.com>
2022-12-16 14:21:01 +0100

    landing page v1

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,37 @@
 <template>
-    <div class="h-screen flex flex-col items-center justify-center bg-blue-400">
-        <div class="alert alert-success shadow-lg max-w-lg mb-10">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>Tailwind is working guys!</span>
+    <div class="h-screen flex flex-col justify-center bg-accent">
+        <div>
+            <h1 class="text-center text-xl text-white">
+                Welcome to
+            </h1>
+            <h1 class="text-4xl text-white text-center font-medium">
+                Find My Friends !
+            </h1>
+        </div>
+        <div class="mt-10 flex justify-center h-1/3 items-center gap-14 text-white">
+            <div class="w-1/12">
+                <GlobeEuropeAfricaIcon />
+                <div class="text-center text-2xl">Explore</div>
+            </div>
+            <div class="w-1/12">
+                <MapPinIcon />
+                <div class="text-center text-2xl">Save</div>
+            </div>
+            <div class="w-1/12">
+                <UsersIcon />
+                <div class="text-center text-2xl">Share</div>
             </div>
         </div>
-        <h1 class="text-center text-xl text-white">
-            Welcome Home!
-        </h1>
+        <div class="text-center text-white text-4xl">Locations</div>
+        <div class="flex justify-center my-20 gap-5 items-center">
+            <button class="btn btn-info">Sign in</button>
+            <span class="text-white text-center text-xl">or</span>
+            <button class="btn btn-success">Sign up</button>
+        </div>
     </div>
 </template>
+
+<script setup>
+import { MapPinIcon, GlobeEuropeAfricaIcon, UsersIcon } from '@heroicons/vue/24/solid';
+
+</script>
