3675b552b7cac833e20e3c3d549cea42b679ae09
Isma_ <studiogalactik@gmail.com>
2022-12-16 12:54:02 +0100

    style ++

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,22 +1,36 @@
 <template>
-    <div class="bg-primary flex flex-col justify-between items-center w-20 py-5">
-        <div class="avatar w-2/3 h-fit">
-            <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img src="https://via.placeholder.com/500" />
+    <div class="bg-neutral flex flex-col justify-between items-center w-20 py-5">
+        <div class="avatar w-1/2">
+            <div class="w-24 rounded-full">
+                <img src="https://placeimg.com/192/192/people" />
             </div>
         </div>
-        <div>
-            <div>Home</div>
+        <div class="flex flex-col gap-3">
+            <HomeIcon class="h-10 w-10 text-white bg-primary rounded-md p-2 hover:bg-secondary hover:cursor-pointer" />
+            <SquaresPlusIcon class="h-10 w-10 text-white bg-primary rounded-md p-2 hover:bg-secondary hover:cursor-pointer" />
         </div>
         <div>
-            <div class="avatar placeholder">
-                <div class="bg-neutral-focus text-neutral-content rounded-full w-12">
-                    <span>MX</span>
-                </div>
+            <div class="dropdown dropdown-top">
+                <label tabindex="0" class="avatar placeholder m-1 hover:cursor-pointer">
+                    <div class="bg-neutral-focus text-neutral-content rounded-full w-12">
+                        <span>MX</span>
+                    </div>
+                </label>
+                <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Profile</a></li>
+                    <li><a>Friends</a></li>
+                    <li><a class="text-error">Sign out</a></li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { HomeIcon, SquaresPlusIcon } from '@heroicons/vue/24/solid'
+
+export default {
+    components: { HomeIcon, SquaresPlusIcon }
+}
+
 </script>
