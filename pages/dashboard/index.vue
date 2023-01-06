86b73215a92d3cf0c391db7b5a279636a4bbc749
macsimtv <maximelecouturier@gmail.com>
2023-01-06 15:47:47 +0100

    🔨 Try adding Middleware routes

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,9 +1,16 @@
-<template>
-  Dashboard
-</template>
 
 <script setup>
+import { protectedRoute } from "~/lib/middleware";
+
 definePageMeta({
   layout: "dashboard",
 });
+
+onMounted(() => {
+  protectedRoute();
+});
 </script>
+
+<template>
+  Dashboard
+</template>
